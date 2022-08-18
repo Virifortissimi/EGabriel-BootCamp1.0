462cd3e8da8ea44e7af533c48604d3c6fa511a59
Onimcodes <Abrahammicheal55@yahoo.com>
2022-08-18 09:31:15 +0100

    restructured folder

--- Micheal/First-Project/ProductsCRUDAPI/ProductsCRUDAPI/Controllers/WeatherForecastController.cs
+++ Micheal/First-Project/ProductsCRUDAPI/ProductsCRUDAPI/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApiWithMongoDB.Controllers
+namespace ProductsCRUDAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Micheal/First-Project/ProductsCRUDAPI/ProductsCRUDAPI/WeatherForecast.cs
+++ Micheal/First-Project/ProductsCRUDAPI/ProductsCRUDAPI/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace ProductCRUDAPI
+namespace ProductsCRUDAPI
 {
     public class WeatherForecast
     {
